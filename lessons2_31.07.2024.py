55ba5d5634efa80faff2126c811b5d86dbaf3472
ZabavanSQ <zabavnsq@mail.ru>
2024-08-01 00:25:33 +0300

    Переменые

---
+++ lessons2_31.07.2024.py
@@ -0,0 +1,5 @@
+kolichestvo_vpolnenh_dz = 12
+kolichestvo_hour = 1.5
+nazvanie_kyrsa = 'Python'
+vremja_na_zadanie = kolichestvo_hour / kolichestvo_vpolnenh_dz
+print('Курс:' ,nazvanie_kyrsa + ',','всего задач:',str(kolichestvo_vpolnenh_dz) + ',','затрачено часов:',str(kolichestvo_hour) + ',','среднее время выполнения',vremja_na_zadanie,'часа.')
