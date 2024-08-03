c9d19fb585050c75dd18b79a9e3fadb0b0a4ece2
ZabavanSQ <zabavnsq@mail.ru>
2024-08-03 16:03:50 +0300

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -2,6 +2,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students_mod = list(students)
+students_mod.sort()
 n = grades.__len__()
 print('Количество записей в журнале =' , n)
 jurnal = dict({students_mod[0]: round(sum(grades[0]) / grades[0].__len__(),1)})
