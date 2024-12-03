6cca7af982caa8de7bcd8344235f1f32156c9730
ZabavanSQ <zabavnsq@mail.ru>
2024-12-03 11:38:17 +0300

    тест_базовое задание

---
+++ Obchii_test.py
@@ -0,0 +1,15 @@
+# 1st_program
+a = (9 ** 0.5) * 5
+print(a)
+# 2nd_program
+print( 9.99 > 9.98 and 1000 != 1000.1)
+# 3rd_program
+d1 = 2 * 2 + 2
+d2 = 2 * (2+2)
+print(d1)
+print(d2)
+print(d1 == d2)
+#4th_program
+c = '123.456'
+ff= float(c)
+print (int((ff % 1) * 10))
